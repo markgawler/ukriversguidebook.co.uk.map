6a1ae5ba4a862b01dcf258dbedb05787d9f6b3ec
Mark <markg@wler.co.uk>
2023-01-16 07:28:47 +0000

    Align MySQL version with production

--- docker/start-env.sh
+++ docker/start-env.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-docker-compose -f stack.yml up
+docker-compose -f stack2.yml up
+
+# To connect to image for debuging
+#
+# $ docker exec -it docker_joomladb_1 bash
+# or
+# $ docker exec -it docker_joomla_1 bash
