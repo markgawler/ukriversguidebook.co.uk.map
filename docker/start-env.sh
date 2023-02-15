c40cc5273ef0a23715bb0065d5c8ebf9630dfeed
Mark <markg@wler.co.uk>
2023-02-15 19:53:24 +0000

    Docker config for debugging

--- docker/start-env.sh
+++ docker/start-env.sh
@@ -7,3 +7,10 @@ docker-compose -f stack.yml up
 # $ docker exec -it docker_joomladb_1 bash
 # or
 # $ docker exec -it docker_joomla_1 bash
+
+# Debug:
+# # Build the Image including xdebug
+# $ docker build -t joomla-debug:3.10.11 .
+#
+# $ docker-compose -f stack-debug.yml up
+#
