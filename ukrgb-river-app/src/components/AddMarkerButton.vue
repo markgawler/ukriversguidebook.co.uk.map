91ec4a7bcaa7d02269c70bec911cd978f373318b
Mark <markg@wler.co.uk>
2023-06-09 15:03:37 +0100

    Remove redundant code copied in

--- ukrgb-river-app/src/components/AddMarkerButton.vue
+++ ukrgb-river-app/src/components/AddMarkerButton.vue
@@ -4,7 +4,6 @@ import { useStore } from "vuex";
 const store = useStore();
 
 const onClickControl = () => {
-  console.log('clicked Add')
     const center = store.getters["mapParameters/getCenter"]
     const point = {
       description: "",
@@ -16,33 +15,8 @@ const onClickControl = () => {
 </script>
     
 <template>
-  <div>
-    <div class="radio-toolbar">
-      <input id="radio1" type="radio" name="radios" @click="onClickControl" />
-      <label for="radio1">Add </label>
-    </div>
-  </div>
+      <button  @click="onClickControl" >New</button>
 </template>
 
 <style>
-.radio-toolbar {
-  display: flex;
-}
-
-.radio-toolbar input[type="radio"] {
-  display: none;
-}
-
-.radio-toolbar label {
-  display: inline-block;
-  background-color: #ddd;
-  padding: 4px 11px;
-  font-family: Arial;
-  font-size: 16px;
-  cursor: pointer;
-}
-
-.radio-toolbar input[type="radio"]:checked+label {
-  background-color: #bbb;
-}
 </style>
